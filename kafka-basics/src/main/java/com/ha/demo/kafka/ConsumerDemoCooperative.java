0198c4948c3dd92e861d143d02a4b462c2a5fc9c
hasiabewardana <hasi.prasanga@gmail.com>
2024-03-23 23:34:20 +0530

    Enabled local bootstrap server.

--- kafka-basics/src/main/java/com/ha/demo/kafka/ConsumerDemoCooperative.java
+++ kafka-basics/src/main/java/com/ha/demo/kafka/ConsumerDemoCooperative.java
@@ -28,13 +28,13 @@ public class ConsumerDemoCooperative {
         Properties properties = new Properties();
 
 //        Local cluster
-//        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("bootstrap.servers", "localhost:9092");
 
 //        Conduktor playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        /*properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
         properties.setProperty("security.protocol", "SASL_SSL");
         properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"13uqy3GgJawT980fHGLB8j\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIxM3VxeTNHZ0phd1Q5ODBmSEdMQjhqIiwib3JnYW5pemF0aW9uSWQiOjY3NzczLCJ1c2VySWQiOjc3OTUwLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI2NTIyMmE5MC04NTcwLTQzM2ItOTA5Yy0xMTMyOGEwMWNlODYifX0.C3NS0kbOWBgvOddL3rD2r5RY2UV72h0ctRS05PoIc2w\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
+        properties.setProperty("sasl.mechanism", "PLAIN");*/
 
 //        Set consumer properties
         properties.setProperty("key.deserializer", StringDeserializer.class.getName());
