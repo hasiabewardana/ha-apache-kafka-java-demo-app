292a4b033bd9d343b030518e11072c64ac297e44
hasiabewardana <hasi.prasanga@gmail.com>
2024-04-27 18:13:07 +0530

    Implemented wikimedia producer.

---
+++ kafka-producer-wikimedia/src/main/java/com/ha/demo/kafka/WikimediaChangesProducer.java
@@ -0,0 +1,40 @@
+package com.ha.demo.kafka;
+
+import com.launchdarkly.eventsource.ConnectStrategy;
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.StreamException;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws StreamException, InterruptedException {
+        String bootstrapServer = "127.0.0.1:9092";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the producer.
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder((ConnectStrategy) eventHandler);
+        EventSource eventSource = builder.build();
+
+        // start the producer in another thread
+        eventSource.start();
+
+        // we produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
